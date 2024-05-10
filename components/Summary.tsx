6238aa9cfa8d1ec5a8899790fc71a767557ba655
facsert <facsert@outlook.com>
2024-05-10 22:50:34 +0800

    todo
    1.add summary count
    
    Signed-off-by: facsert <facsert@outlook.com>

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,10 +1,14 @@
-import { Card } from "@/components/ui/card";
+"use client"
+
+import { useTodo } from "@/lib/data"
 
 export default function Summary() {
+    const { todos } = useTodo();
+    const completed = todos.filter((todo) => todo.completed).length;
     return (
         <div className="flex flex-row justify-center items-center w-full h-full">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Total Todos: 3 / 5
+                {`Total Todos: ${completed} / ${todos.length}`}
             </h1>
         </div>
     )
